46090796cb3cf8a012944022dec4b19300a3b2a4
HunterFloyd <86444533+HunterFloyd@users.noreply.github.com>
2021-07-09 17:46:49 -0500

    adding pokeminList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert("Hello World");
-let favoriteFood=("cheeseburger");
-document.write(favoriteFood);
+let pokemonList= [
+  {
+    name: 'Charizard',
+    height: 1.7,
+    types: ['fire', 'flying']
+  }
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    types: ['electric']
+  }
+  {
+    name: 'Arcanine',
+    height: 1.9,
+    types: ['fire']
+  }
+];
